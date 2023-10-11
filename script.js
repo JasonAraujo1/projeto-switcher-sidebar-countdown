b0824ef6c67638054f37e31a6c5e3f4b9d14314c
Jason A. <you@example.com>
2023-10-11 12:04:56 -0300

    index initial adjusts

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal() {
+    document.getElementById("modal").style.display = "block";
+}
+
+// Função para fechar o modal
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
