9415c2dbc6398017cf33797b34e20655487a009a
Jason A. <you@example.com>
2023-10-16 15:09:30 -0300

    click fixed nav menu lateral js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-const button = document.querySelector("#inscrever")
+//ABRIR E FECHAR MODAL
+const button = document.querySelector('#inscrever')
 const modal = document.querySelector("dialog") 
-const buttonFechar = document.querySelector("#fechar")
+const buttonFechar = document.querySelector('#fechar')
 
 button.onclick = function () {
     modal.showModal()
@@ -10,3 +11,20 @@ button.onclick = function () {
 buttonFechar.onclick = function (){
     modal.close()
 }
+
+
+
+//CLICK SELEÇÃO FIXA ICONS DA UL LI
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectIconLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo'))
+        this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectIconLink)
+)
+
+
