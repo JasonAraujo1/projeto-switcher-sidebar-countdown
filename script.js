eee0779216c315a91d36b6afe19804ed8abc2f18
Jason A. <you@example.com>
2023-10-16 19:22:18 -0300

    display side menu phone config

--- script.js
+++ script.js
@@ -28,3 +28,10 @@ menuItem.forEach((item)=>
 )
 
 
+//EXPANDIR MENU
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
