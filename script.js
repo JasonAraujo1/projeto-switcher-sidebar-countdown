ee3bb6d852934f45fabbc47210dba797ed8ace4b
Jason A. <you@example.com>
2023-10-16 19:55:09 -0300

    display phone size elements adjust

--- script.js
+++ script.js
@@ -35,3 +35,13 @@ var menuSide = document.querySelector('.menu-lateral')
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandir')
 })
+
+
+
+//EXPANDIR MENU DISPLAY PHONE
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandirPhone')
+})
