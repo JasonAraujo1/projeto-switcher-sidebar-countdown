ab5c4718ba831ddac6e859548b349c155575ddb4
Jason A. <you@example.com>
2023-10-17 01:39:07 -0300

    countdown js add code and fix css elements

--- script.js
+++ script.js
@@ -45,3 +45,33 @@ var menuSide = document.querySelector('.menu-lateral')
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandirPhone')
 })
+
+
+
+
+//COUNTDOWN
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+const finishCountDown = new Date(`${currentYear + 1}-10-17 00:00:00`);
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = finishCountDown - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor((diff / 1000 / 60 / 60) % 24);
+    const m = Math.floor((diff / 1000 / 60) % 60);
+    const s = Math.floor((diff / 1000) % 60);
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
