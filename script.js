b29a190ae22c763963376981254f9b58390983dd
Jason A. <you@example.com>
2023-10-17 12:22:53 -0300

    color dark mode adjust

--- script.js
+++ script.js
@@ -75,3 +75,39 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 
+
+/*OBS EXPLICAÇÃO
+O código JavaScript é responsável por criar um contador regressivo que
+ mostra o tempo restante até uma data específica.
+
+São definidas quatro variáveis: days, hours, minutes e seconds, que 
+representam elementos HTML onde os valores do contador serão exibidos.
+
+O ano atual é obtido com new Date().getFullYear() e armazenado na variável
+ currentYear.
+
+A data de término do contador regressivo é definida como o próximo ano, 
+17 de outubro, às 00:00:00, e armazenada em finishCountDown.
+
+A função updateCountdown() é criada para calcular e atualizar os valores 
+do contador.
+
+Dentro de updateCountdown(), a diferença de tempo entre finishCountDown e
+ a hora atual é calculada para determinar o tempo restante.
+
+A diferença é convertida em dias, horas, minutos e segundos, que são usados
+ para atualizar elementos HTML correspondentes.
+
+Verificações são realizadas para formatar valores menores que 10 com um 
+zero à esquerda.
+
+A função updateCountdown() é chamada a cada segundo usando setInterval() 
+para manter o contador atualizado dinamicamente.
+
+Como resultado, a página exibe um contador regressivo em tempo real que 
+mostra os dias, horas, minutos e segundos restantes até a data de término definida.
+
+Esse código é útil para criar contadores regressivos em páginas da web 
+para eventos, promoções ou qualquer outra situação em que você deseja exibir
+ o tempo restante.
+*/
